b93f8f0a2b6777ae7aea9ad4964b9f7aac48ce10
dda410 <dimitri1215@gmail.com>
2018-12-27 19:29:11 +0000

    Added division of individuals from migrant pool into groups

--- Assignments/assignment2/src/Main.java
+++ Assignments/assignment2/src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -24,14 +25,50 @@ public class Main {
         return population;
     }
 
+    private static ArrayList<ArrayList> computeGroups(ArrayList<Individual> population, int groupSize, boolean largeGroup) {
+        ArrayList<ArrayList> allGroups = new ArrayList<>();
+        ArrayList<Individual> group = new ArrayList<>();
+        for (Individual individual : population) {
+            if(individual.isLargeGroupGene() == largeGroup) {
+                group.add(individual.clone());
+            }
+            if(group.size() == groupSize) {
+                allGroups.add(group);
+                group = new ArrayList<>();
+            }
+        }
+        return allGroups;
+    }
+
+    private static void printGroups(ArrayList<ArrayList> groups) {
+        int i = 1;
+        for (ArrayList<Individual> group : groups) {
+            System.out.println("Printing group number " + i);
+            for (Individual ind : group) {
+                System.out.println(ind);
+            }
+            i++;
+        }
+    }
+
 
     public static void main(String[] args) {
+        // 1: Generate population of N individuals. Equiprobable distribution of genotypes.
         ArrayList<Individual> population = generatePopulation();
         System.out.println(population);
         Collections.shuffle(population);
         System.out.println(population);
         System.out.println(population.size());
-        int occurrences = Collections.frequency(population, SELFISH_SMALL);
-        System.out.print(occurrences);
+        // 2: Divide into groups
+        ArrayList<ArrayList> smallGroups = computeGroups(population, SMALL_GROUP_SIZE, false);
+        System.out.println("Printing small groups with size: " + smallGroups.size());
+        printGroups(smallGroups);
+        ArrayList<ArrayList> largeGroups = computeGroups(population, LARGE_GROUP_SIZE, true);
+        System.out.println("Printing large groups with size: " + largeGroups.size());
+        printGroups(largeGroups);
+
+
+//        int occurrences = Collections.frequency(population, SELFISH_SMALL);
+//        System.out.print(occurrences);
     }
 }
