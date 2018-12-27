b4aa87f81a2e75f2bcfe45a2ab1ddea03e08ad42
dda410 <dimitri1215@gmail.com>
2018-12-27 23:09:58 +0000

    Added reproduction step draft

--- Assignments/assignment2/src/Main.java
+++ Assignments/assignment2/src/Main.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -9,8 +8,12 @@ public class Main {
     private static final int TIME_IN_GROUP = 4;
     private static final int SMALL_GROUP_SIZE = 4;
     private static final int LARGE_GROUP_SIZE = 40;
+    private static final double BONUS_RESOURCE = 0.05;
+    private static final double SMALL_GROUP_RESOURCE = 1 * SMALL_GROUP_SIZE;
+    private static final double LARGE_GROUP_RESOURCE = (1 + LARGE_GROUP_SIZE / SMALL_GROUP_SIZE * BONUS_RESOURCE) * LARGE_GROUP_SIZE;
+    private static final double DEATH_RATE = 0.1;
     private static final Individual SELFISH_SMALL = new Individual(true, false);
-    private static final Individual SEFLISH_LARGE = new Individual(true, true);
+    private static final Individual SELFISH_LARGE = new Individual(true, true);
     private static final Individual COOPERATIVE_SMALL = new Individual(false, false);
     private static final Individual COOPERATIVE_LARGE = new Individual(false, true);
 
@@ -51,6 +54,33 @@ public class Main {
         }
     }
 
+    private static double computeNextGeneration(int thisGeneSize, double thisGeneGrowth, double thisGeneConsumption,
+                                             int thatGeneSize, double thatGeneGrowth, double thatGeneConsumption, double resources) {
+        double resourceShare = ( (thisGeneSize * thisGeneGrowth * thisGeneConsumption) /
+                (thatGeneSize * thatGeneGrowth * thatGeneConsumption) ) * resources;
+        return thisGeneSize * (resourceShare / thisGeneConsumption) - DEATH_RATE * thisGeneSize;
+    }
+
+    private static void reproductionStep(ArrayList<ArrayList> groups, boolean largeGroup) {
+        double resources = largeGroup ? LARGE_GROUP_RESOURCE : SMALL_GROUP_RESOURCE;
+        Individual cheater = largeGroup ? SELFISH_LARGE : SELFISH_SMALL;
+        Individual cooperator = largeGroup ? COOPERATIVE_LARGE : COOPERATIVE_SMALL;
+        int groupSize = largeGroup ? LARGE_GROUP_SIZE : SMALL_GROUP_SIZE;
+        for (int i = 0; i < TIME_IN_GROUP; i++) {
+            for (ArrayList<Individual> group : groups) {
+                double groupResources = (1 + group.size() / SMALL_GROUP_SIZE * BONUS_RESOURCE) * group.size();
+                int numberOfCheaters = Collections.frequency(group, cheater);
+                int numberOfCooperators = Collections.frequency(group, cooperator);
+                double nextNumberOfCheaters = computeNextGeneration(numberOfCheaters, Individual.GROWTH_SELFISH, Individual.CONSUMPTION_SELFISH,
+                        numberOfCooperators, Individual.GROWTH_COOPERATIVE, Individual.CONSUMPTION_COOPERATIVE, groupResources);
+                double nextNumberOfCooperators = computeNextGeneration(numberOfCooperators, Individual.GROWTH_COOPERATIVE, Individual.CONSUMPTION_COOPERATIVE,
+                        numberOfCheaters, Individual.GROWTH_SELFISH, Individual.CONSUMPTION_SELFISH, groupResources);
+                System.out.println("Cheaters now: " + numberOfCheaters + " Future: " + nextNumberOfCheaters);
+                System.out.println("Cooperators: " + numberOfCooperators + " Future: " + nextNumberOfCooperators);
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         // 1: Generate population of N individuals. Equiprobable distribution of genotypes.
@@ -66,7 +96,10 @@ public class Main {
         ArrayList<ArrayList> largeGroups = computeGroups(population, LARGE_GROUP_SIZE, true);
         System.out.println("Printing large groups with size: " + largeGroups.size());
         printGroups(largeGroups);
-
+        System.out.println(LARGE_GROUP_RESOURCE);
+        System.out.println(SMALL_GROUP_RESOURCE);
+        reproductionStep(largeGroups, true);
+        reproductionStep(smallGroups, false);
 
 //        int occurrences = Collections.frequency(population, SELFISH_SMALL);
 //        System.out.print(occurrences);
